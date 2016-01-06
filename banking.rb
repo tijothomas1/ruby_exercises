7329059fd5ea4ed26682a35de25bbb12142ebd03
TIJO THOMAS <tt.opt.ins@gmail.com>
2016-01-05 20:42:13 -0600

    final banking code

--- banking.rb
+++ banking.rb
@@ -4,10 +4,22 @@ puts "*******************************"
 accounts = []
 
 5.times do
-  print "enter account holder's first name: "
+  print "Enter account holder's first name: "
   first_name = gets.chomp
 
-  accounts << first_name
+  print "Enter account holder's last name: "
+  last_name = gets.chomp
+
+  print "Enter account holder's email: "
+  email = gets.chomp
+
+  acct_num = rand(1000000000..9999999999)
+
+  puts ""
+  puts "-----------------------------"
+  puts ""
+
+  accounts << {first_name: first_name, last_name: last_name}
 end
 
 p accounts
